8ce015122b3b87c526a4ac9a5e2fda18bacf57df
Todd Bartoszkiewicz <todd.bart@gmail.com>
2025-10-11 01:27:06 -0400

    Add file dialog window to save file

---
+++ Module1/module1_portfolio_milestone_bartoszkiewicz.py
@@ -0,0 +1,30 @@
+# Todd Bartoszkiewicz
+# CSC515: Foundations of Computer Vision
+# Module 1: Portfolio Milestone
+import cv2
+import tkinter as tk
+from tkinter import filedialog
+
+if __name__ == '__main__':
+    img = cv2.imread('shutterstock130285649--250.jpg')
+    root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.asksaveasfilename(
+        defaultextension=".jpg",
+        filetypes=[("JPG files", "*.jpg")],
+        title="Save File As"
+    )
+
+    if file_path:
+        try:
+            cv2.imwrite(file_path, img)
+            print(f"File saved to: {file_path}")
+        except IOError as ioe:
+            print(f"Error saving file: {ioe}")
+    else:
+        cv2.imwrite('numbers.jpg', img)
+
+    cv2.imshow('image_window', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
